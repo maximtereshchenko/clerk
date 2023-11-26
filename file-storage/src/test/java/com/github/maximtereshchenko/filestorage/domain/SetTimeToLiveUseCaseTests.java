1e1ab4033b19f73d3f00aebe2846b4f457efc40a
maximtereshchenko <tereshchenko.xini@gmail.com>
2023-11-26 12:43:10 +0100

    Set time to live (file do not exist)

--- file-storage/src/test/java/com/github/maximtereshchenko/filestorage/domain/SetTimeToLiveUseCaseTests.java
+++ file-storage/src/test/java/com/github/maximtereshchenko/filestorage/domain/SetTimeToLiveUseCaseTests.java
@@ -1,6 +1,7 @@
 package com.github.maximtereshchenko.filestorage.domain;
 
 import com.github.maximtereshchenko.filestorage.api.FileStorageModule;
+import com.github.maximtereshchenko.filestorage.api.exception.CouldNotFindFile;
 import com.github.maximtereshchenko.test.ClasspathFileExtension;
 import com.github.maximtereshchenko.test.ManualClock;
 import com.github.maximtereshchenko.test.PredictableUUIDExtension;
@@ -11,16 +12,18 @@ import java.io.ByteArrayOutputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ExtendWith({ClasspathFileExtension.class, PredictableUUIDExtension.class, FileStorageModuleExtension.class})
 final class SetTimeToLiveUseCaseTests extends UseCaseTest {
 
     @Test
-    void givenFileExist_whenSetTimeToLive_thenFileIsNotCleaned(
+    void givenFileExists_whenSetTimeToLive_thenFileIsNotCleaned(
             Path file,
             UUID id,
             ManualClock clock,
@@ -35,6 +38,12 @@ final class SetTimeToLiveUseCaseTests extends UseCaseTest {
         module.writeFile(id, outputStream);
 
         assertThat(outputStream.toString(StandardCharsets.UTF_8)).isEqualTo(Files.readString(file));
+    }
 
+    @Test
+    void givenFileDoNotExist_whenSetTimeToLive_thenCouldNotFindFileThrown(UUID id, FileStorageModule module) {
+        assertThatThrownBy(() -> module.setTimeToLive(id, Instant.MAX))
+                .isInstanceOf(CouldNotFindFile.class)
+                .hasMessageContaining(id.toString());
     }
 }
